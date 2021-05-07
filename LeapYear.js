ceb244fe23ea0473500e86934b42d2550df5e84d
ganesh-48 <ganeshsrananavare@gmail.com>
2021-05-07 22:53:10 +0530

    ADDED: Leap year or not

---
+++ LeapYear.js
@@ -0,0 +1,12 @@
+/*@Description- Check the year is leap year or not*/
+
+function leapYear(year) {
+
+    if((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+        console.log(year+ "This is leap year");
+    } else {
+        console.log(year+ "This is not a leap year");
+    }
+}
+const year = prompt("Enter a Year: ");
+leapYear(year);
